1a8f5e826f88b427016925a60e1622a94c317a78
Niall Moran <niall@tryingtorun.com>
2023-11-17 23:01:53 +0000

    alpha release

--- app/src/main/java/com/niallermoran/charteasysampleapp/MainActivity.kt
+++ app/src/main/java/com/niallermoran/charteasysampleapp/MainActivity.kt
@@ -7,7 +7,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
